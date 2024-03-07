81053de51f3ef02eff1952168b9fd63a2e56edfd
Miguel Bio <bio.235890@caloocan.sti.edu.ph>
2024-03-07 15:40:30 +0800

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+import React, {Component} from 'react';
+import AppNavbar from './components/AppNavbar';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello, Im Miguel Bio</h1>
+      <AppNavbar/>
       
     </div>
   );

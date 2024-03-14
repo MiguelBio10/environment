c9fc7e4d2c36ac58403d7f3899dea333b2576331
Miguel Bio <bio.235890@caloocan.sti.edu.ph>
2024-03-14 16:38:01 +0800

    ongoing to find a error of my codes

--- client/src/components/FavoriteCharacter.js
+++ client/src/components/FavoriteCharacter.js
@@ -1,151 +1,48 @@
 import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, Button, Input } from 'reactstrap';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { v4 as uuidv4 } from 'uuid'; // Importing v4 function from uuid package
+import { connect } from 'react-redux';
+import { getItems } from '../actions/animesActions';
+import PropTypes from 'prop-types';
 
 class FavoriteCharacter extends Component {
-    state = {
-        characters: [
-            { id: uuidv4(), name: 'Naruto' },
-            { id: uuidv4(), name: 'Luffy' },
-            { id: uuidv4(), name: 'Saitama'},
-            { id: uuidv4(), name: 'Ichigo'}
-        ],
-        searchText: '', // State for search text
-        editingCharacterId: null, // State to track which character is being edited
-        editedCharacterName: '' // State to store edited character name
-    };
-
-    // Function to handle adding a new character
-    handleAddCharacter = () => {
-        const name = prompt('Enter Character');
-        if (name) {
-            this.setState(state => ({
-                characters: [...state.characters, { id: uuidv4(), name }]
-            }));
-        }
-    }
-
-    // Function to handle removing a character
-    handleRemoveCharacter = id => {
-        this.setState(state => ({
-            characters: state.characters.filter(character => character.id !== id)
-        }));
-    }
-
-    // Function to handle starting edit of a character
-    handleEditCharacter = (id, name) => {
-        this.setState({
-            editingCharacterId: id,
-            editedCharacterName: name
-        });
-    }
-
-    // Function to handle editing character name
-    handleEditedCharacterNameChange = e => {
-        this.setState({ editedCharacterName: e.target.value });
-    }
-
-    // Function to handle saving edited character name
-    handleSaveEditedCharacterName = id => {
-        const { editedCharacterName, characters } = this.state;
-        const updatedCharacters = characters.map(character =>
-            character.id === id ? { ...character, name: editedCharacterName } : character
-        );
-        this.setState({
-            characters: updatedCharacters,
-            editingCharacterId: null,
-            editedCharacterName: ''
-        });
-    }
-
-    // Function to handle search input change
-    handleSearchChange = e => {
-        this.setState({ searchText: e.target.value });
+    componentDidMount() {
+        this.props.getItems();
     }
 
     render() {
-        const { characters, searchText, editingCharacterId, editedCharacterName } = this.state;
-
-        // Filter characters based on search text
-        const filteredCharacters = characters.filter(character =>
-            character.name.toLowerCase().includes(searchText.toLowerCase())
-        );
+        const { characters } = this.props.animes;
 
         return (
-            <Container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                {/* Button to add a new character */}
+            <Container>
                 <Button
                     color="dark"
-                    style={{ marginBottom: '1rem', alignSelf: 'flex-start', backgroundColor: '#ff9800', borderColor: '#ff9800' }}
-                    onClick={this.handleAddCharacter}
+                    style={{ marginBottom: '2rem' }}
+                    onClick={() => {
+                        const name = prompt('Enter Character');
+                        if (name) {
+                            // Dispatch action to add character here
+                        }
+                    }}
                 >
                     Add Character
                 </Button>
-
-                {/* Search bar */}
-                <Input
-                    type="text"
-                    placeholder="Search Characters..."
-                    value={searchText}
-                    onChange={this.handleSearchChange}
-                    style={{ marginBottom: '1rem', width: '100%' }}
-                />
-
-                {/* List of characters */}
-                <ListGroup style={{ width: '100%', justifyContent: 'flex-start' }}>
+                <ListGroup>
                     <TransitionGroup className="favorite-characters">
-                        {filteredCharacters.map(({ id, name }) => (
+                        {characters && characters.map(({ id, name }) => (
                             <CSSTransition key={id} timeout={500} classNames="fade">
-                                <ListGroupItem style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                    <div style={{ flex: 1 }}>
-                                        {editingCharacterId === id ? (
-                                            <Input
-                                                type="text"
-                                                value={editedCharacterName}
-                                                onChange={this.handleEditedCharacterNameChange}
-                                            />
-                                        ) : (
-                                            <>
-                                                {/* Character name */}
-                                                {name}
-                                            </>
-                                        )}
-                                    </div>
-
-                                    {/* Button to remove a character */}
+                                <ListGroupItem>
                                     <Button
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
-                                        onClick={() => this.handleRemoveCharacter(id)}
+                                        onClick={() => {
+                                            // Dispatch action to delete character here
+                                        }}
                                     >
                                         x
                                     </Button>
-
-                                    {/* Edit button */}
-                                    {!editingCharacterId ? (
-                                        <Button
-                                            className="edit-btn"
-                                            color="info"
-                                            size="sm"
-                                            style={{ backgroundColor: '#cccccc', borderColor: '#cccccc' }}
-                                            onClick={() => this.handleEditCharacter(id, name)}
-                                        >
-                                            Edit
-                                        </Button>
-                                    ) : (
-                                        <Button
-                                            className="save-btn"
-                                            color="success"
-                                            size="sm"
-                                            style={{ backgroundColor: '#4caf50', borderColor: '#4caf50' }}
-                                            onClick={() => this.handleSaveEditedCharacterName(id)}
-                                        >
-                                            Update
-                                        </Button>
-                                    )}
-
+                                    {name}
                                 </ListGroupItem>
                             </CSSTransition>
                         ))}
@@ -156,4 +53,17 @@ class FavoriteCharacter extends Component {
     }
 }
 
-export default FavoriteCharacter;
+FavoriteCharacter.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    animes: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => {
+    console.log(state.animes); // Log the state to check if it contains the characters array
+    return {
+        animes: state.animes
+    };
+};
+
+
+export default connect(mapStateToProps, { getItems })(FavoriteCharacter);
--- client/src/reducers/animesReducer.js
+++ client/src/reducers/animesReducer.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from '../actions/types';
 const initialState = {
-    character: [
+    characters: [ // Change property name from 'character' to 'characters'
         { id: uuidv4(), name: 'Naruto' },
         { id: uuidv4(), name: 'Luffy' },
         { id: uuidv4(), name: 'Saitama'},

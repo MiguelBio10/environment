229cb7d41fd5e2ddf0cda5f54e6acc23e4e73000
Miguel Bio <bio.235890@caloocan.sti.edu.ph>
2024-03-15 17:01:03 +0800

    Implementing Redux-Delete Item

--- client/src/components/FavoriteCharacter.js
+++ client/src/components/FavoriteCharacter.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { connect } from 'react-redux';
-import { getItems } from '../actions/animesActions';
+import { connect } from 'react-redux'; // Import connect
+import { getItems, deleteItem } from '../actions/animesActions';
 import PropTypes from 'prop-types';
 
 class FavoriteCharacter extends Component {
@@ -10,23 +10,16 @@ class FavoriteCharacter extends Component {
         this.props.getItems();
     }
 
+    onDeleteClick = id => {
+        console.log(id); // Check if the correct ID is received
+        this.props.deleteItem(id);
+    }
+
     render() {
         const { characters } = this.props.animes;
 
         return (
             <Container>
-                <Button
-                    color="dark"
-                    style={{ marginBottom: '2rem' }}
-                    onClick={() => {
-                        const name = prompt('Enter Character');
-                        if (name) {
-                            // Dispatch action to add character here
-                        }
-                    }}
-                >
-                    Add Character
-                </Button>
                 <ListGroup>
                     <TransitionGroup className="favorite-characters">
                         {characters && characters.map(({ id, name }) => (
@@ -36,9 +29,7 @@ class FavoriteCharacter extends Component {
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
-                                        onClick={() => {
-                                            // Dispatch action to delete character here
-                                        }}
+                                        onClick={() => this.onDeleteClick(id)}
                                     >
                                         x
                                     </Button>
@@ -55,15 +46,15 @@ class FavoriteCharacter extends Component {
 
 FavoriteCharacter.propTypes = {
     getItems: PropTypes.func.isRequired,
+    deleteItem: PropTypes.func.isRequired, // Make sure deleteItem is also included in propTypes
     animes: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) => {
-    console.log(state.animes); // Log the state to check if it contains the characters array
-    return {
-        animes: state.animes
-    };
-};
+const mapStateToProps = (state) => ({
+    animes: state.animes
+});
 
-
-export default connect(mapStateToProps, { getItems })(FavoriteCharacter);
+export default connect(
+    mapStateToProps,
+    { getItems, deleteItem }
+)(FavoriteCharacter);
--- client/src/reducers/animesReducer.js
+++ client/src/reducers/animesReducer.js
@@ -1,7 +1,8 @@
 import { v4 as uuidv4 } from 'uuid';
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+
 const initialState = {
-    characters: [ // Change property name from 'character' to 'characters'
+    characters: [
         { id: uuidv4(), name: 'Naruto' },
         { id: uuidv4(), name: 'Luffy' },
         { id: uuidv4(), name: 'Saitama'},
@@ -9,14 +10,18 @@ const initialState = {
     ]
 };
 
-export default function(state = initialState, action){
-    switch(action.type){
+export default function(state = initialState, action) {
+    switch(action.type) {
         case GET_ITEMS:
-            return{
+            return {
                 ...state
-            }
-            default:
-                return state;
-
+            };
+        case DELETE_ITEM:
+            return {
+                ...state,
+                characters: state.characters.filter(character => character.id !== action.payload)
+            };
+        default:
+            return state;
     }
 }

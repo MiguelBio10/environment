527b0eb7a45bad1c6de35699057ef8aa5d4de47c
Miguel Bio <bio.235890@caloocan.sti.edu.ph>
2024-04-17 17:04:06 +0800

    using the JWT registration and get the token.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose =require('mongoose');
 const path = require('path');
+const config = require('config');
 
 
 const app = express ();
@@ -9,7 +10,7 @@ const app = express ();
 app.use(express.json());
 
 
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 //connect to mongo
 mongoose
